568e0ead67a1438a5b8df467c404c1fb6a3e22b4
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-25 13:55:43 +0200

    Chore : Subject list

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Subject;
+use App\Form\SubjectType;
+use App\Repository\SubjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SubjectController extends AbstractController
 {
     #[Route('/subject', name: 'app_subject')]
-    public function index(): Response
+    public function index(SubjectRepository $subjectRepository): Response
     {
+        $subjects = $subjectRepository->findBy([],["label" => "ASC"]);
         return $this->render('subject/index.html.twig', [
-            'controller_name' => 'SubjectController',
+            'allsubjects' => $subjects,
         ]);
     }
 }
