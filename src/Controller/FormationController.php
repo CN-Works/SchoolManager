ffd23b449857b13d9ed7dfae0dce83fab679a66c
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-25 10:00:07 +0200

    Feat : Formation list

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formation;
+use App\Repository\FormationRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+        // Getting all formations
+        $formations = $formationRepository->findBy([],["label" => "ASC"]);
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'allformations' => $formations,
+        ]);
+    }
+
+    
+
+    #[Route('/formation/{id}', name: 'formation_show')]
+    public function show(Formation $formation): Response {
+        
+        return $this->render('formation/show_formation.html.twig', [
+            "formation" => $formation,
         ]);
     }
 }
