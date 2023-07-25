7785b261b26a9f784aa221514419d970a7eaec47
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-25 11:52:44 +0200

    Feat : Create/Update/Delete Student

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -22,6 +22,48 @@ class StudentController extends AbstractController
         ]);
     }
 
+    #[Route('/student/new', name: 'new_student')]
+    #[Route('/student/{id}/edit', name: 'edit_student')]
+    public function new_edit(Student $student = null,Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$student) {
+            $student = new Student;
+        }
+
+        $form = $this->createForm(StudentType::class, $student);
+
+        $form->handleRequest($request);
+
+        // Checks if form inputs are in a valid type
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Affecting the form data into the object
+            $student = $form->getData();
+
+            $entityManager->persist($student);
+            // Saving in db
+            $entityManager->flush();
+
+            // Redirection to all societies
+            return $this->redirectToRoute("app_student");
+        }
+
+        return $this->render('student/new_edit.html.twig', [
+            'formAddEditStudent' => $form,
+            "edit" => $student->getId(),
+        ]);
+    }
+
+    #[Route('/student/{id}/delete', name: 'delete_student')]
+    public function delete(StudentRepository $studentRepository, EntityManagerInterface $entityManager, $id)
+    {   
+        $student = $studentRepository->find(($id));
+        $entityManager->remove($student);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_student');
+    }
+
     #[Route('/student/{id}', name: 'show_student')]
     public function show(Student $student): Response {
         
