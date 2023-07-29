be180682cb6ba9e082a823645e8a731a6898f470
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-29 23:24:22 +0200

    Style : Register form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,21 +20,33 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('username', TextType::class)
+            ->add('email', EmailType::class,[
+                "attr" => [
+                    "class" => "register-form-inputbox",
+                ]
+            ])
+            ->add('username', TextType::class,[
+                "attr" => [
+                    "class" => "register-form-inputbox",
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
-                ]),
+                    ]),
                 ],
+                "attr" => [
+                    "class" => "register-form-checkboxinput",
+                ],
+                'row_attr' => ['class' => 'register-form-checkbox'],
             ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => "Passwords don't match !",
                 'mapped' => false,
-                'options' => ['attr' => ['class' => 'password-field']],
+                'options' => ['attr' => ['class' => 'password-field register-form-inputbox']],
                 'required' => true,
                 'first_options'  => ['label' => 'Password'],
                 'second_options' => ['label' => 'Repeat password',],
