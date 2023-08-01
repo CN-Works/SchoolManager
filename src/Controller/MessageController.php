6f16c0802aa025d9f32a4afc2fecf0b4f0d1c946
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-08-02 00:53:17 +0200

    Feat : Show message

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -14,10 +14,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class MessageController extends AbstractController
 {
     #[Route('/message', name: 'app_message')]
+    #[IsGranted('ROLE_USER')]
     public function index(MessageRepository $messageRepository): Response
     {   
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+        ]);
+    }
+
+    #[Route('/message/{id}', name: 'show_message')]
+    #[IsGranted('ROLE_USER')]
+    public function show(Message $message, EntityManagerInterface $entityManager): Response {
+        $message->setReaded(true);
+        $entityManager->persist($message);
+        // Saving in db
+        $entityManager->flush();
+        return $this->render('message/show_message.html.twig', [
+            "message" => $message,
         ]);
     }
 }
